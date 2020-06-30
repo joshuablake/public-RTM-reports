199b47c05a80be92940208c4a698146eb6c4b149
pjbirrell <paul.birrell@mrc-bsu.cam.ac.uk>
2020-06-30 16:49:30 +0100

    Paul's first attempt at updating the web report.

--- generate.R
+++ generate.R
@@ -1,21 +1,14 @@
-file.loc <- "~/public-RTM-reports/"
-proj.dir <- "~/RTM"
+## proj.dir <- "~/RTM"
+out.dir <- "/project/pandemic_flu/Wuhan_Coronavirus/real-time-mcmc/model_runs/20200626/base_varserology6day_matrices_20200619_deaths"
 
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) < 2) {
-	date.data <- format(lubridate::today() - lubridate::days(1), "%Y%m%d")
-	regions <- c("East_of_England", "London", "Midlands",
-									  "North_East_and_Yorkshire", "North_West",
-									  "South_East", "South_West")
-	nr <- length(regions)
-}
-source(file.path(proj.dir, "config.R"))
+load(file.path(out.dir, "tmp.RData"))
+file.loc <- "~/public-RTM-reports/"
 
 wd <- getwd()
 setwd(file.loc)
 index_file <- "iframe.html"
 output_file <- paste0(date.data, ".html")
-rmarkdown::render(file.path(Rfile.loc, 'report-updated.Rmd'), output_dir = file.loc,
+rmarkdown::render(file.path(proj.dir, 'R/output/report-updated.Rmd'), output_dir = file.loc,
 				  clean = FALSE, intermediates_dir = file.loc,
 				  output_options = list(
 	mathjax = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"
