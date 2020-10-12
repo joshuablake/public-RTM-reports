47a55487a0a36c8c72f54f0f428d1d21f4ae2372
Joshua Blake <joshuab@mrc-bsu.cam.ac.uk>
2020-10-12 10:28:36 +0100

    Update report

--- generate.R
+++ generate.R
@@ -1,8 +1,9 @@
 ## proj.dir <- "~/RTM"
 out.dir <- "/project/pandemic_flu/Wuhan_Coronavirus/real-time-mcmc/model_runs/20201009/NoPrev_shortsero_ifr_60cutoff6day_matrices_20201009_deaths"
 
-load(file.path(out.dir, "tmp.RData"))
 file.loc <- "~/public-RTM-reports"
+proj.dir <- "~/real-time-mcmc"
+Rfile.loc <- file.path(proj.dir, "R/output")
 
 wd <- getwd()
 setwd(file.loc)
@@ -10,7 +11,7 @@ external <- TRUE
 index_file <- "iframe.html"
 output_file <- paste0(lubridate::today(), ".html")
 rmarkdown::render(file.path(proj.dir, 'R/output/report-updated.Rmd'), output_dir = file.loc,
-				  clean = FALSE, intermediates_dir = file.loc,
+				  intermediates_dir = file.loc,
 				  output_options = list(
 	mathjax = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"
 					),
