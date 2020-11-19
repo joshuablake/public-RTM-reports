9bca932bf85b6beb3c5fa5f3165e663bf87546c3
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-11-19 15:33:58 +0000

    Latest report

--- plots.R
+++ plots.R
@@ -1,7 +1,7 @@
 library(lubridate)
 library(plotly)
 
-out.dir <- "/project/pandemic_flu/Wuhan_Coronavirus/real-time-mcmc/model_runs/20201108/NoPrev_relax_shortsero_ifr_60cutoff6day_matrices_20201106_deaths"
+out.dir <- "/project/pandemic_flu/Wuhan_Coronavirus/real-time-mcmc/model_runs/20201115/NoPrev_relax_shortsero_cm4_ifr_60cutoff_matrices_20201115_deaths"
 out.dir.orig <- out.dir
 load(file.path(out.dir, "tmp.RData"))
 proj.dir.new <- "~/COVID/real-time-mcmc"
